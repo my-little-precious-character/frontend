5e514e08c06d6c9571198e92ea2c65470005585a
Youngho Cho <dominoyh5@gmail.com>
2025-05-04 09:56:57 +0900

    chore: explit port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
+    port: 5174,
     allowedHosts: [
       'my-character.cho0h5.org',
     ]
